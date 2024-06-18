4eb2f202c973209af1e9562613e178f0f826e3f3
Hirwa Clement Rhodin <h1rhodin@gmail.com>
2024-06-18 13:08:36 +0200

    project added

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('Application Hello'),
+          ),
+        ),
+        body: Center(
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
